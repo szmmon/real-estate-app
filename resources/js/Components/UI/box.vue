304d98a1aa06e640b624538d724597d3c90d0a52
Szymon Ziemianek <szymonziemianek@gmail.com>
2024-05-01 18:02:01 +0200

    added stylig for showing listing

--- resources/js/Components/UI/box.vue
+++ resources/js/Components/UI/box.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="border border-gray-200 dark:border-gray-700 rounded-md p-4 shadow-sm dark:text-gray-200">
+    <div class="font-medium text-gray-500">
+        <slot name="header"/>
+    </div>
         <slot/>
     </div>
 </template>
--- resources/js/Pages/Listing/show.vue
+++ resources/js/Pages/Listing/show.vue
@@ -1,9 +1,29 @@
 <template>
-    <ListingAddress :listing="listing" />
+    <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box class="col-span-12 md:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">No images</div>
+        </Box>
+        <div class="md:col-span-5 flex flex-col gap-3">
+        <Box>
+            <template #header>Basic info</template>
+            <Price :price="listing.price" class="text-2xl bold"/>
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAddress :listing="listing" class="text-gray" />
+        </Box>
+        <Box>
+            <template #header>Offer</template>
+            Make an offer
+        </Box>
+        </div>
+        
+    </div>
 </template>
 
 <script setup>
+import Box from "@/Components/UI/box.vue";
 import ListingAddress from "@/Components/listingAddress.vue";
+import ListingSpace from "@/Components/listingSpace.vue";
+import Price from "@/Components/price.vue";
 defineProps({
     listing: Object,
 });
